9a80130efa8282c837458a6e1e64362006f54cb2
Chase McCarthy <chase@code0100fun.com>
2019-11-13 23:53:35 -0600

    WIP

--- app/components/episode-show.js
+++ app/components/episode-show.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['h-full', 'flex'],
+  classNames: ['h-full', 'flex', 'flex-col', 'sm:flex-row'],
 });
--- app/components/guest-item.js
+++ app/components/guest-item.js
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'li',
-  classNames: ['bg-white', 'border-b', 'border-gray-400', 'flex', 'flex-row', 'p-8'],
+  classNames: ['bg-white', 'border-b', 'border-gray-400', 'flex', 'flex-col', 'xs:flex-row', 'p-8'],
 });
